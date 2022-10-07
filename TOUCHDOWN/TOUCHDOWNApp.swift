dcbcb35335f4f3d5bf1bae9573a65489cb776f01
soroushamdg <soroushmpour@gmail.com>
2022-10-07 11:47:39 -0400

    Initial Commit

---
+++ TOUCHDOWN/TOUCHDOWNApp.swift
@@ -0,0 +1,17 @@
+//
+//  TOUCHDOWNApp.swift
+//  TOUCHDOWN
+//
+//  Created by Soro on 2022-10-07.
+//
+
+import SwiftUI
+
+@main
+struct TOUCHDOWNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
